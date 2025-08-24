01a7c487433def5204befd3833be3bce501b6335
jake <tkwk1205@naver.com>
2025-08-24 18:26:25 +0900

    백준 1912

---
+++ ts/백준/1912.ts
@@ -0,0 +1,21 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+
+// const input = [10, 10, -4, 3, 1, 5, 6, -35, 12, 21, -1];
+// const input = [10, 2, 1, -4, 3, 4, -4, 6, 5, -5, 1];
+// const input = [5, -1, -2, -3, -4, -5];
+
+const N = input[0];
+const arr = input.slice(1, 1 + N);
+
+let best = arr[0];
+let cur = arr[0];
+
+for (let i = 1; i < N; i++) {
+  cur = Math.max(arr[i], cur + arr[i]);
+
+  best = Math.max(best, cur);
+}
+
+console.log(best);
