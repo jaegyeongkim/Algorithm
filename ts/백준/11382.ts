51eff0410024144cbaaf49296877d89682e29b82
jake <tkwk1205@naver.com>
2025-09-01 00:14:42 +0900

    백준: 11382

---
+++ ts/백준/11382.ts
@@ -0,0 +1,9 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+
+const a = input[0];
+const b = input[1];
+const c = input[2];
+
+console.log(a + b + c);
