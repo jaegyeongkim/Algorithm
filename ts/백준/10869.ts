8824416ae016dc7e85709eaa52582296ff1afa2d
jake <tkwk1205@naver.com>
2025-08-31 23:58:06 +0900

    백준: 10869

---
+++ ts/백준/10869.ts
@@ -0,0 +1,12 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+
+const a = input[0];
+const b = input[1];
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
