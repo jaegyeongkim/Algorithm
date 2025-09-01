c98ffdec5ba965097bdda18aabcb694d5bc35ba3
jake <tkwk1205@naver.com>
2025-09-01 23:25:55 +0900

    백준: 14681

---
+++ ts/백준/14681.ts
@@ -0,0 +1,20 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+
+const x = input[0];
+const y = input[1];
+
+const whatYourSquare = (x: number, y: number): void => {
+  if (x > 0 && y > 0) {
+    console.log(1);
+  } else if (x < 0 && y > 0) {
+    console.log(2);
+  } else if (x < 0 && y < 0) {
+    console.log(3);
+  } else if (x > 0 && y < 0) {
+    console.log(4);
+  }
+};
+
+whatYourSquare(x, y);
