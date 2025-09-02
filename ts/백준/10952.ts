cdc40bed7444b077e33495ddc34bf27951ddbb80
jake <tkwk1205@naver.com>
2025-09-02 23:43:46 +0900

    백준: 10952

---
+++ ts/백준/10952.ts
@@ -0,0 +1,13 @@
+// LINK: https://www.acmicpc.net/problem/10952
+
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+
+// const input = [1, 1, 2, 3, 3, 4, 9, 8, 5, 2, 0, 0];
+
+const N = input.length;
+
+for (let i = 0; i < (N - 2) / 2; i++) {
+  console.log(input[i * 2] + input[i * 2 + 1]);
+}
