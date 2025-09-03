fc908aec290ed20dfb4e6f0b4e03ed3ea0c4edf5
jake <tkwk1205@naver.com>
2025-09-04 00:40:48 +0900

    백준: 11654

---
+++ ts/백준/11654.ts
@@ -0,0 +1,8 @@
+// LINK: https://www.acmicpc.net/problem/11654
+
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf-8").trim();
+// const input = "A";
+
+console.log(input.codePointAt(0));
