fe2d9bd98179ecb3b359c1fe1166032f58ad7b1b
jaegyeongkim <60080671+jaegyeongkim@users.noreply.github.com>
2025-09-29 14:38:17 +0900

    백준: 3009

--- ts/백준/3009.ts
+++ ts/백준/3009.ts
@@ -1,1 +1,28 @@
 // LINK: https://www.acmicpc.net/problem/3009
+
+import * as fs from "fs";
+
+const xArr: Record<string, number> = {};
+const yArr: Record<string, number> = {};
+
+fs.readFileSync(0, "utf8")
+  .trim()
+  .split(/\s+/)
+  .forEach((num: string, i) => {
+    if (i % 2 === 0) {
+      xArr[num] = (xArr[Number(num)] || 0) + 1;
+    } else {
+      yArr[num] = (yArr[Number(num)] || 0) + 1;
+    }
+  });
+
+const answer: string[] = [];
+
+Object.entries(xArr).forEach(([key, value]) => {
+  if (value === 1) answer.push(key);
+});
+Object.entries(yArr).forEach(([key, value]) => {
+  if (value === 1) answer.push(key);
+});
+
+console.log(answer.join(" "));
