464e907a09f322d81f30c6f5221de8bf5442c069
jaegyeongkim <60080671+jaegyeongkim@users.noreply.github.com>
2025-10-01 08:55:01 +0900

    백준: 10101

--- ts/백준/10101.ts
+++ ts/백준/10101.ts
@@ -1,20 +1,20 @@
 // https://www.acmicpc.net/problem/10101
-
 import * as fs from "fs";
 
 const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
 const a1 = input[0];
 const a2 = input[1];
 const a3 = input[2];
+
 if (a1 + a2 + a3 !== 180) {
   console.log("Error");
 } else if (a1 === 60 && a2 === 60 && a3 === 60) {
   console.log("Equilateral");
-} else if (a1 === a2 && a2 === a3) {
+} else if (a1 === a2) {
   console.log("Isosceles");
-} else if (a2 === a3 && a3 === a1) {
+} else if (a3 === a1) {
   console.log("Isosceles");
-} else if (a3 === a1 && a1 === a2) {
+} else if (a2 === a3) {
   console.log("Isosceles");
 } else {
   console.log("Scalene");
