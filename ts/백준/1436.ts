185e9ced9abdcfba63626feac3e6ec43a235d143
jake <tkwk1205@naver.com>
2025-10-19 00:50:17 +0900

    백준: 1436

--- ts/백준/1436.ts
+++ ts/백준/1436.ts
@@ -1,3 +1,19 @@
+// LINK: https://www.acmicpc.net/problem/1436https://www.acmicpc.net/problem/1436
+
 import * as fs from "fs";
 
-const input = fs.readFileSync(0, "utf8").trim().split(/\s+/);
+const input = Number(fs.readFileSync(0, "utf8").trim().split(/\s+/));
+
+let count = 0;
+let num = 666;
+
+while (1) {
+  if (String(num).includes("666")) {
+    count++;
+  }
+
+  if (count === input) break;
+  num++;
+}
+
+console.log(num);
