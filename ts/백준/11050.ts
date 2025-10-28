28c1b0153c774708a0bd729e0a4f1ca25e6a4677
jaegyeongkim <60080671+jaegyeongkim@users.noreply.github.com>
2025-10-28 13:56:21 +0900

    백준: 11050

---
+++ ts/백준/11050.ts
@@ -0,0 +1,18 @@
+// LINK: https://www.acmicpc.net/problem/11050
+
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+
+const N = input[0];
+const K = input[1];
+
+const facotiral = (n: number) => {
+  let result = 1;
+  for (let i = 1; i < n + 1; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+console.log(facotiral(N) / facotiral(K) / facotiral(N - K));
