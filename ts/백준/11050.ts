9655a23c683e33b291e3053e0136a0f00d724079
jaegyeongkim <60080671+jaegyeongkim@users.noreply.github.com>
2025-10-28 14:05:22 +0900

    Optimize factorial calculation using memoization

--- ts/백준/11050.ts
+++ ts/백준/11050.ts
@@ -6,13 +6,15 @@ const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
 
 const N = input[0];
 const K = input[1];
+const arr = [1];
 
 const facotiral = (n: number) => {
   let result = 1;
   for (let i = 1; i < n + 1; i++) {
     result *= i;
+    arr.push(result);
   }
   return result;
 };
 
-console.log(facotiral(N) / facotiral(K) / facotiral(N - K));
+console.log(facotiral(N) / arr[N - K] / arr[K]);
