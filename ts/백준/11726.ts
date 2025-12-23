2dbdad7b63a7be7990f51ab71493014b6e58c434
jaegyeongkim <60080671+jaegyeongkim@users.noreply.github.com>
2025-12-23 15:13:44 +0900

    백준: 11726 정답 풀이 (DP 로 품)

--- ts/백준/11726.ts
+++ ts/백준/11726.ts
@@ -4,37 +4,13 @@ import * as fs from "fs";
 
 const N = Number(fs.readFileSync(0, "utf8"));
 
-const combination = (n: number, r: number): number => {
-  if (r === 0) return 1;
-  if (r === 1) return n;
-  if (n === r) return 1;
+const arr = [0, 1, 2];
 
-  let result = 1;
-
-  for (let i = 1; i <= n; i++) {
-    result *= i;
+if (N > 2) {
+  for (let n = 3; n <= N; n++) {
+    const result = (arr[n - 2] + arr[n - 1]) % 10007;
+    arr.push(result);
   }
-  for (let i = 1; i <= r; i++) {
-    result /= i;
-  }
-  for (let i = 1; i <= n - r; i++) {
-    result /= i;
-  }
-
-  return result;
-};
-
-let sum = 0;
-let oddCount = N % 2 === 0 ? 0 : 1;
-
-while (1) {
-  const n = oddCount + (N - oddCount) / 2;
-  const r = oddCount;
-
-  sum += combination(n, r);
-
-  oddCount += 2;
-  if (oddCount > N) break;
 }
 
-console.log(sum % 10007);
+console.log(arr[N]);
