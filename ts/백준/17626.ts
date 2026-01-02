d4924ec62fed0b7b769263c8584cf87b2d1686dc
jake <tkwk1205@naver.com>
2026-01-03 01:01:07 +0900

    백준: 17626 정답

--- ts/백준/17626.ts
+++ ts/백준/17626.ts
@@ -2,24 +2,39 @@
 
 import * as fs from "fs";
 
-let N = Number(fs.readFileSync(0, "utf8"));
+const N = Number(fs.readFileSync(0, "utf8"));
 
-let n = N;
-let cnt = 0;
-let arr: number[] = [];
+const maxNum = Math.floor(Math.sqrt(N));
+let minCount = 4;
 
-while (n > 0) {
-  const result = Math.floor(Math.sqrt(n));
+for (let a = maxNum; a > 0; a--) {
+  const A = Math.pow(a, 2);
 
-  n -= Math.pow(result, 2);
-  cnt += 1;
-  arr.push(result);
-  if (cnt > 4) {
-    n = N;
-    arr = [arr[0] - 1];
-    n -= Math.pow(arr[0], 2);
-    cnt = 1;
+  if (A === N) {
+    minCount = 1;
+    break;
+  }
+
+  for (let b = a; b > 0; b--) {
+    const AB = A + Math.pow(b, 2);
+
+    if (AB === N) {
+      minCount = minCount > 2 ? 2 : minCount;
+      break;
+    } else if (AB > N) {
+      continue;
+    }
+
+    for (let c = b; c > 0; c--) {
+      const ABC = AB + Math.pow(c, 2);
+
+      if (ABC === N) {
+        minCount = minCount > 3 ? 3 : minCount;
+        break;
+      } else if (ABC > N) {
+        continue;
+      }
+    }
   }
 }
-
-console.log(cnt);
+console.log(minCount);
