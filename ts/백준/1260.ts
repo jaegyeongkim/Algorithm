81cccee8e9c0d43a30145ed50c839e40b7eba836
jake <tkwk1205@naver.com>
2026-01-07 00:56:57 +0900

    백준: 1260

--- ts/백준/1260.ts
+++ ts/백준/1260.ts
@@ -7,75 +7,51 @@ const input = fs.readFileSync(0, "utf8").trim().split("\n").map(String);
 const [N, M, V] = input[0].split(" ").map(Number);
 const arr = input.splice(1, M);
 
-const dfsMap: number[][] = Array.from({ length: N + 1 }, () =>
-  new Array(N + 1).fill(0)
-);
-
-const bfsMap: number[][] = Array.from({ length: N + 1 }, () =>
-  new Array(N + 1).fill(0)
-);
+const dfsMap = Array.from({ length: N + 1 }, () => new Array(N + 1).fill(0));
+const bfsMap = Array.from({ length: N + 1 }, () => new Array(N + 1).fill(0));
 
 arr.forEach((unit) => {
-  const [x, y] = unit.split(" ").map(Number);
-  dfsMap[x][y] = 1;
-  dfsMap[y][x] = 1;
-  bfsMap[x][y] = 1;
-  bfsMap[y][x] = 1;
+  const [r, c] = unit.split(" ").map(Number);
+  dfsMap[r][c] = 1;
+  dfsMap[c][r] = 1;
+  bfsMap[r][c] = 1;
+  bfsMap[c][r] = 1;
 });
 
-const dfsIdx = dfsMap[V].findIndex((v) => v === 1);
-const dfs = [[V, dfsIdx]];
-const dfsResult = [V, dfsIdx];
+const dfs = [V];
+const dfsAnswer: number[] = [];
+const dfsVisited = new Array(N + 1).fill(0);
 
-while (dfs) {
-  const [x, y] = dfs.pop() as number[];
+while (dfs.length) {
+  const cur = dfs.pop() as number;
+  if (dfsVisited[cur]) continue;
+  dfsVisited[cur] = 1;
+  dfsAnswer.push(cur);
 
-  dfsMap[x][y] = 0;
-  dfsMap[y][x] = 0;
-
-  const idx = dfsMap[y].findIndex((v) => v === 1);
-  if (idx === -1) {
-    break;
-  }
-
-  dfs.push([y, idx]);
-  if (!dfsResult.includes(y)) {
-    dfsResult.push(y);
+  for (let next = N; next > 0; next--) {
+    if (dfsMap[cur][next] === 1 && !dfsVisited[next]) {
+      dfs.push(next);
+    }
   }
 }
 
-const bfsIdx = bfsMap[V].findIndex((v) => v === 1);
-const bfs = [[V, bfsIdx]];
-const bfsResult = [V, bfsIdx];
+const bfs = [V];
+const bfsAnswer: number[] = [];
+const bfsVisited = new Array(N + 1).fill(0);
 
-while (bfs) {
-  const [x, y] = bfs.shift() as number[];
+while (bfs.length) {
+  const cur = bfs.shift() as number;
 
-  bfsMap[x][y] = 0;
-  bfsMap[y][x] = 0;
+  if (bfsVisited[cur]) continue;
+  bfsVisited[cur] = 1;
+  bfsAnswer.push(cur);
 
-  bfsMap[x].filter((v, idx) => {
-    if (v === 1) {
-      bfs.push([x, idx]);
-      bfsMap[x][idx] = 0;
-      bfsMap[idx][x] = 0;
+  for (let next = 1; next <= N; next++) {
+    if (bfsMap[cur][next] === 1 && !bfsVisited[next]) {
+      bfs.push(next);
     }
-  });
-
-  bfsMap[y].filter((v, idx) => {
-    if (v === 1) {
-      bfs.push([y, idx]);
-      bfsMap[y][idx] = 0;
-      bfsMap[idx][y] = 0;
-    }
-  });
-
-  if (!bfsResult.includes(y)) {
-    bfsResult.push(y);
   }
-
-  if (bfs.length === 0) break;
 }
 
-console.log(dfsResult.join(" "));
-console.log(bfsResult.join(" "));
+console.log(dfsAnswer.join(" "));
+console.log(bfsAnswer.join(" "));
