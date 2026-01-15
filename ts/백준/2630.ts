3b96d5aaf1631b46d22b6c4d9aca88a71eaa9281
jaegyeongkim <60080671+jaegyeongkim@users.noreply.github.com>
2026-01-15 13:59:31 +0900

    백준: 2630 정답 풀이

--- ts/백준/2630.ts
+++ ts/백준/2630.ts
@@ -13,14 +13,40 @@ let blueCount = 0;
 let seperates: number[][][] = [origin];
 
 while (1) {
-  if (N === 1) break;
-  const n = N / 2;
+  if (N === 1) {
+    for (const sep of seperates) {
+      if (sep[0][0] === 0) whiteCount++;
+      else blueCount++;
+    }
+    break;
+  }
 
+  if (N === Number(input[0])) {
+    let isSame = 1;
+    const firstNum = seperates[0][0][0];
+    for (let r = 0; r < N; r++) {
+      for (let c = 0; c < N; c++) {
+        if (firstNum !== seperates[0][r][c]) {
+          isSame = 0;
+        }
+      }
+    }
+    if (isSame) {
+      if (firstNum === 0) {
+        whiteCount++;
+      } else {
+        blueCount++;
+      }
+      break;
+    }
+  }
+
+  const n = N / 2;
   const part: number[][][] = [];
 
   seperates.forEach((seperate) => {
-    for (let i = 0; i < N; i += n) {
-      for (let j = 0; j < N; j += n) {
+    for (let i = 0; i < seperate.length; i += n) {
+      for (let j = 0; j < seperate.length; j += n) {
         const abc: number[][] = [];
         let isSame = 1;
         const firstNum = seperate[i][j];
@@ -50,7 +76,6 @@ while (1) {
   });
 
   seperates = part;
-
   N = n;
 }
 
